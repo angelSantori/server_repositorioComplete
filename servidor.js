0ffb822970fc03f14f9a23bcdb5e0dbc0495cb73
angelSantori <antonyo.da.mm@gmail.com>
2023-10-13 09:32:37 -0700

    Integración de minitipos

--- servidor.js
+++ servidor.js
@@ -1,3 +1,4 @@
+//Estructura del servidor
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -11,3 +12,22 @@ http.createServer(function (request, response){
         filePath = './index.html';
     }
 });
+
+
+//Tipo de archivos que puede leer el sitio web
+var extname = String(path.extname(filePath)).toLowerCase();
+var contentType = 'text/html';
+
+var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    //woof, ttf, eot, otf = application/ font-woof, font-ttf,vnd.ms-fontobject, font-otf
+    //svg = application /image/svg+xml
+
+};
