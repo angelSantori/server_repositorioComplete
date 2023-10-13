28d8c7270eac85baa0091d1ff4198542b9029351
angelSantori <antonyo.da.mm@gmail.com>
2023-10-13 09:48:43 -0700

    Manejar errores

--- servidor.js
+++ servidor.js
@@ -33,3 +33,24 @@ var mimeTypes = {
     '.otf': 'application/font-otf',
     '.svg': 'application/image/svg+xml',    
 };
+
+contentType = mimeTypes[extname] || 'application/octet-stream';
+
+fs.readFile(filePath, function (error, content) {
+    if (error) {
+        if (error.code == 'ENOENT') {
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200, {'Content-Type': contentType});
+                response.end(content, 'utf-8');
+            }); 
+        }
+        else {
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error:' + error.code + '..\n');
+        }        
+    }
+    else {
+        response.writeHead(200, {'Content-Type': contentType});
+        response.end(content, 'utf-8');
+    }    
+});
